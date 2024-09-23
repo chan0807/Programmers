a64594e60b2671abd4cee754be52576c6c84d4bb
Gunchukzae <156566900+Gunchukzae@users.noreply.github.com>
2024-09-23 10:48:20 +0900

    <알고리즘 스터디>(해쉬): 완주하지 못한 선수, 전화번호 목록

---
+++ AlgorithmKing.cpp
@@ -0,0 +1,9 @@
+#include "AlgorithmKing.h"
+
+int main()
+{
+    vector<string> participant = { "leo", "kiki", "eden" };
+    vector<string> completion = {"eden", "kiki"};
+
+    string answer = HashOne(participant, completion);
+}
