30339952ec183e1b79bafadddf45e610d30a7cd2
kjh0727 <kjh0727@midasit.com>
2024-09-27 09:34:42 +0900

    기능개발

---
+++ 스택큐/기능개발.cpp
@@ -0,0 +1,27 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    vector<int> v_stack;
+    for (int i = 0; i < progresses.size(); i++)
+    {
+        int time = (100 - progresses[i]) / speeds[i];
+        if ((100 - progresses[i]) % speeds[i] != 0) time += 1;
+
+        if (v_stack.size() == 0) { v_stack.push_back(time); }
+        else if (v_stack[0] < time) {
+            answer.push_back(v_stack.size());
+            v_stack.clear();
+            v_stack.push_back(time);
+        }
+        else v_stack.push_back(time);
+    }
+    if (v_stack.size() != 0)
+        answer.push_back(v_stack.size());
+    return answer;
+}
