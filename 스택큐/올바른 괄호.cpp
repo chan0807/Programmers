610e702e6ce083f537d77f2e5251250b16d26279
kjh0727 <kjh0727@midasit.com>
2024-09-30 08:34:31 +0900

    올바른 괄호 & 프로세스

---
+++ 스택큐/올바른 괄호.cpp
@@ -0,0 +1,25 @@
+﻿#include<string>
+#include <iostream>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+bool solution(string s)
+{
+    stack<char> stc;
+
+    for (const auto& c : s)
+    {
+        if (c == ')')
+        {
+
+            if (stc.empty()) return false;
+            else  stc.pop();
+        }
+        else stc.push(c);
+
+    }
+    if (stc.size() != 0) return false;
+
+    return true;
+}
