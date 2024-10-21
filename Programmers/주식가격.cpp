6fd0adde15bda38ae2ec2b694a908e34ad7cffd8
chan0807 <zzzzz0807@gmail.com>
2024-10-21 21:08:22 +0900

    <수정> (build error) : 함수명 중복되는 경우때문에 namespace 처리함

--- Programmers/주식가격.cpp
+++ Programmers/주식가격.cpp
@@ -4,47 +4,49 @@
 
 using namespace std;
 
-//deque 이용 방식
-vector<int> solution(vector<int> prices) {
-    vector<int> answer;
-    deque<int> dq;
+namespace 주식가격 {
+    //deque 이용 방식
+    vector<int> solution(vector<int> prices) {
+        vector<int> answer;
+        deque<int> dq;
 
-    for (int i : prices)
-        dq.push_back(i);
+        for (int i : prices)
+            dq.push_back(i);
 
-    while (!dq.empty())
-    {
-        int cur = dq.front();
-        dq.pop_front();
-
-        int cnt = 0;
-        for (int i : dq)
+        while (!dq.empty())
         {
-            cnt++;
-            if (cur > i) break;
+            int cur = dq.front();
+            dq.pop_front();
+
+            int cnt = 0;
+            for (int i : dq)
+            {
+                cnt++;
+                if (cur > i) break;
+            }
+
+            answer.push_back(cnt);
         }
 
-        answer.push_back(cnt);
+        return answer;
     }
 
-    return answer;
-}
+    /*
+        vector만 이용해서 푸는 경우
+        루프를 최대한 간소화해서 속도 측면에서 우월함
+    */
+    vector<int> solution2(vector<int> prices) {
+        vector<int> answer(prices.size());
 
-/*
-    vector만 이용해서 푸는 경우 
-    루프를 최대한 간소화해서 속도 측면에서 우월함
-*/
-vector<int> solution2(vector<int> prices) {
-    vector<int> answer(prices.size());
-
-    for (size_t i = 0; i < prices.size(); ++i) {
-        int cnt = 0;
-        for (size_t j = i + 1; j < prices.size(); ++j) {
-            cnt++;
-            if (prices[i] > prices[j]) break;
+        for (size_t i = 0; i < prices.size(); ++i) {
+            int cnt = 0;
+            for (size_t j = i + 1; j < prices.size(); ++j) {
+                cnt++;
+                if (prices[i] > prices[j]) break;
+            }
+            answer[i] = cnt;
         }
-        answer[i] = cnt;
-    }
 
-    return answer;
+        return answer;
+    }
 }
