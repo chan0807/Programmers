9a1fe473aac2c0521e4e175d21689652398e7d19
chan0807 <zzzzz0807@gmail.com>
2024-10-24 18:03:54 +0900

    완전탐색 - 피로도

---
+++ Programmers/피로도.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+namespace 피로도 {
+	int solution(int k, vector<vector<int>> dungeons) {
+		int maxCnt = 0;
+		sort(dungeons.begin(), dungeons.end());
+		do {
+			int health = k, cnt = 0;
+			for (auto& dungeon : dungeons) {
+				if (health >= dungeon[0]) {
+					health -= dungeon[1];
+					cnt++;
+				}
+			}
+			maxCnt = max(maxCnt, cnt);
+		} while (next_permutation(dungeons.begin(), dungeons.end()));
+
+		return maxCnt;
+	}
+}
