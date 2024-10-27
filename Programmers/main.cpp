6712d742d3dc9cd14dc99acd1db5321b4d8e9153
gunchukzae@gmail.com <gunchukzae@gmail.com>
2024-10-27 16:30:02 +0900

    <탐욕법>: 체육복

--- Programmers/main.cpp
+++ Programmers/main.cpp
@@ -3,16 +3,16 @@
 
 using namespace std;
 
-namespace 피로도 {
-    int solution(int k, vector<vector<int>> dungeons);
+namespace 체육복 {
+    int solution(int n, vector<int> lost, vector<int> reserve);
 }
-
 int main()
 {
-    vector<vector<int>> dungeons = { {80, 20}, {50, 40}, {30, 10} };
-    int k = 80;
+    int n = 6;
+    vector<int> lost = { 1, 3 };
+    vector<int> reserve = { 2,4 };
 
-    int answer = 피로도::solution(k, dungeons);
+    int answer = 체육복::solution(n, lost, reserve);
 
     return 0;
 }
---
+++ Programmers/체육복.cpp
@@ -0,0 +1,61 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+namespace 체육복 {
+    void CalculateMax(vector<bool>& aHasStudent, vector<bool>& aReserveUsed, const vector<int>& reserve, int nInitialCount, int& nMaxResult) {
+        nMaxResult = max(nMaxResult, nInitialCount);
+
+        for (int i = 0; i < reserve.size(); ++i) {
+            int num = reserve[i];
+
+            if (num - 2 >= 0 && !aHasStudent[num - 2] && !aReserveUsed[i]) {
+                aHasStudent[num - 2] = true;   
+                aReserveUsed[i] = true;        
+                CalculateMax(aHasStudent, aReserveUsed, reserve, nInitialCount + 1, nMaxResult);
+                aHasStudent[num - 2] = false;  
+                aReserveUsed[i] = false;    
+            }
+
+            if (num < aHasStudent.size() && !aHasStudent[num] && !aReserveUsed[i]) {
+                aHasStudent[num] = true;       
+                aReserveUsed[i] = true;        
+                CalculateMax(aHasStudent, aReserveUsed, reserve, nInitialCount + 1, nMaxResult);
+                aHasStudent[num] = false;     
+                aReserveUsed[i] = false;     
+            }
+        }
+    }
+
+    int solution(int n, vector<int> lost, vector<int> reserve) {
+        int answer = 0;
+
+        vector<bool> aHasStudent(n, true);
+        vector<int> common;
+
+        sort(lost.begin(), lost.end());
+        sort(reserve.begin(), reserve.end());
+
+        set_intersection(lost.begin(), lost.end(), reserve.begin(), reserve.end(), back_inserter(common));
+
+        for (int num : lost) {
+            if (find(common.begin(), common.end(), num) == common.end()) {
+                aHasStudent[num - 1] = false;
+            }
+        }
+
+        int nInitialCount = count(aHasStudent.begin(), aHasStudent.end(), true);
+
+        vector<int> newReserve;
+        set_difference(reserve.begin(), reserve.end(), lost.begin(), lost.end(), back_inserter(newReserve));
+        reserve = newReserve;
+
+        vector<bool> aReserveUsed(reserve.size(), false);
+        CalculateMax(aHasStudent, aReserveUsed, reserve, nInitialCount, answer);
+
+        return answer;
+    }
+}
