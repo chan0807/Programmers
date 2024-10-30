2330e37ebcb7f7a8b3b376e1c87f45a779a7b11a
chan0807 <zzzzz0807@gmail.com>
2024-10-31 01:46:11 +0900

    탐욕법 - 큰 수 만들기

---
+++ Programmers/큰_수_만들기.cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+
+using namespace std;
+
+namespace 큰수만들기 {
+    string solution(string number, int k) {
+        string answer;
+
+        for (char num : number) {
+            while (!answer.empty() && answer.back() < num && k > 0) {
+                answer.pop_back();
+                k--;
+            }
+            answer.push_back(num);
+        }
+     
+        answer.resize(answer.size() - k);
+
+        return answer;
+    }
+}
