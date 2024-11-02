03b06f05aafb35906c1fa1876dcb0b99081d5f18
kimjunho <kimjunho403@gmail.com>
2024-11-02 15:50:30 +0900

    구명보트, 섬연결하기

---
+++ 탐욕법/섬연결하기.cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include<iostream>
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    int front = 0;
+    vector<pair<int, int>> dic_people;
+    sort(people.begin(),people.end());
+       for(int i =people.size()-1 ;i>=0;--i){
+           if(i == front) {answer++; break;} //라스트 원
+           if(i < front) break; 
+           if(people[front] + people[i] <= limit){ // 같이 탐
+             front++;
+             answer++;
+           }
+           else{ //덩치가 탐 
+                answer++;
+           }
+       }
+    return answer;
+}
