83df656c1d11ef182ae47c14009cafbf01ddf58c
kimjunho <kimjunho403@gmail.com>
2024-11-11 00:06:56 +0900

    정수 삼각형

---
+++ dp/정수 삼각형.cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int dp[500][500];
+
+int solution(vector<vector<int>> triangle) {
+    int answer = 0;
+    dp[0][0] = triangle[0][0];
+    for(int i=1;i<triangle.size();i++){
+        for(int j=0;j<=triangle[i].size()-1;j++){
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-1]) + triangle[i][j];
+        }
+    }
+    
+    answer = *max_element(dp[ triangle.size() - 1], dp[ triangle.size() - 1] + triangle[ triangle.size() - 1].size());
+    
+    return answer;
+    
+}
